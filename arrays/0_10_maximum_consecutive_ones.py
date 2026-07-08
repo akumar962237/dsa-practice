cd6c89d1ca17cc97c0f8b41b78508954d86324b5
ajay <akumar962237@gmail.com>
2026-07-08 21:18:38 +0530

    arrays:max consecutive ones

---
+++ arrays/0_10_maximum_consecutive_ones.py
@@ -0,0 +1,16 @@
+#-----------OPTIMAL SOLUTION----------------
+arr = [1, 1, 0, 1, 1, 1,0, 1, 1, 1, 1]
+
+def max_consecutive_ones(arr):
+    max = 0
+    count = 0
+    for i in range(len(arr)):
+        if arr[i] == 1:
+            count += 1
+            if count > max:
+                max = count
+        else:
+            count = 0
+    return max
+
+print(max_consecutive_ones(arr))
