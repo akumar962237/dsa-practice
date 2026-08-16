a19887de2d42380d169ab4a92bc9e935974a8cb3
ajay <akumar962237@gmail.com>
2026-08-16 21:42:59 +0530

    find the nth root of the number

--- Binary_Search/001_binary_search.py
+++ Binary_Search/001_binary_search.py
@@ -16,6 +16,8 @@ class Solution:
             else:
                 high = mid - 1  # Search in left half
         return -1  # Target not found
+    
+
 if __name__ == "__main__":
     a = [3, 4, 6, 7, 9, 12, 16, 17]  # sorted list
     target = 6  # target element to search
@@ -29,9 +31,9 @@ if __name__ == "__main__":
         print("The target is at index:", ind)
 
 
-#
+#---------------
 #RECURSION METHOD
-#
+#----------------
 
 
 class Solution:
